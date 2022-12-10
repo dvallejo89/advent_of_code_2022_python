aa134dcaede0770f8f7075304ceec4b5e2de6241
Daniel Vallejo <daniel.vallejoparis@evolutio.com>
2022-12-10 11:24:14 +0100

    day6 of advent of code

--- day6/Tuning_Trouble_part1.py
+++ day6/Tuning_Trouble_part1.py
@@ -4,25 +4,26 @@ input_str=tuning_trouble_input.input_str
 for count,letter in enumerate(input_str):
     if input_str[count] in (input_str[count+1:count+4]):
         #pass
-        print(f'{input_str[count]} in {input_str[count+1:count+4]}')
+        #print(f'{input_str[count]} in {input_str[count+1:count+4]}')
         list_check=[1]
     else:
         list_check=[0]
     if input_str[count+1] == input_str[count] or input_str[count+1] in input_str[count+2:count+4]:
-        print(f'{input_str[count+1]} == {input_str[count]} or in {input_str[count+2:count+4]}')
+        #print(f'{input_str[count+1]} == {input_str[count]} or in {input_str[count+2:count+4]}')
         list_check.append(1)
     else:
         list_check.append(0)
     if input_str[count+2] in input_str[count:count+1] or input_str[count+2] in input_str[count+3:count+4]:
-        print(f'{input_str[count+1]} in {input_str[count:count+1]} or in {input_str[count+3:count+4]}')
+        #print(f'{input_str[count+1]} in {input_str[count:count+1]} or in {input_str[count+3:count+4]}')
         list_check.append(1)
     else:
         list_check.append(0)
     if input_str[count+3] == input_str[count+4] or input_str[count+3] in input_str[count:count+2]:
-        print(f'{input_str[count+4]} == {input_str[count]} or in {input_str[count:count+2]}')
+        #print(f'{input_str[count+4]} == {input_str[count]} or in {input_str[count:count+2]}')
         list_check.append(1)
     else:
-        list_check.append(1)
+        list_check.append(0)
     
-    print(list_check)
-    break
+    if sum(list_check) == 0:
+        print(f"solution is {count+4}")
+        break
